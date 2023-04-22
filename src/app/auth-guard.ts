a09b8e3d85353fac4605a03219f61b8674438eb3
leofert <leofert.hilario.s@bulsu.edu.ph>
2023-04-22 17:19:46 +0800

    modified:   src/app/app-routing.module.ts 	modified:   src/app/app.module.ts 	modified:   src/app/auth-guard.ts 	new file:   src/app/unauthorized/unauthorized.component.css 	new file:   src/app/unauthorized/unauthorized.component.html 	new file:   src/app/unauthorized/unauthorized.component.spec.ts 	new file:   src/app/unauthorized/unauthorized.component.ts

--- src/app/auth-guard.ts
+++ src/app/auth-guard.ts
@@ -3,14 +3,31 @@ import {
   ActivatedRouteSnapshot,
   createUrlTreeFromSnapshot,
 } from '@angular/router';
-import { map } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { ApiService } from './api.service';
+import { of } from 'rxjs';
 
 export const authGuard = (next: ActivatedRouteSnapshot) => {
   const service = inject(ApiService);
   return service.isLoggedIn().pipe(
-    map((isLoggedIn) =>
-      isLoggedIn ? true : createUrlTreeFromSnapshot(next, ['/', 'login'])
-    )
+    switchMap((isLoggedIn) => {
+      if (!isLoggedIn) {
+        return of(createUrlTreeFromSnapshot(next, ['/', 'login']));
+      } else {
+        return service.getUserDetails().pipe(
+          map((userType) => {
+            const userTypeValue = userType['userType'];
+            const expectedUserType = next.data['userType'];
+            if (userTypeValue === expectedUserType) {
+              console.log('Authorization Granted');
+              return true;
+            } else {
+              console.log('Authorization Denied');
+              return createUrlTreeFromSnapshot(next, ['/', 'unauthorized']);
+            }
+          })
+        );
+      }
+    })
   );
 };
