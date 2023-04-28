6ba54192ec2801471cb5a5e799fab25e04eb63d5
ern-ocampo <ernest.s.ocampo@gmail.com>
2023-04-28 12:34:26 +0800

    - Function for checking if User submitted feedback

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,35 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-vote',
   templateUrl: './vote.component.html',
   styleUrls: ['./vote.component.css']
 })
-export class VoteComponent implements OnInit{
+export class VoteComponent implements OnInit {
+  // Holds all data from CURRICULUM Table
   curriculums: any;
-  
+  // Holds all data from VOTE Table
+  allFeedbacks: any;
   tempUserType: any;
+  // Holds userID from getUserDetails Service
+  userID: any;
+
+  submissions: any = [];
 
   isTableVisible: boolean = true;
   isTableSubmittedVisible: boolean = true;
 
-
-
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
-    this.displayCurriculum();
-
+    this.getAllFeedbacks();
     this.apiService.getUserDetails().subscribe(response => {
       this.tempUserType = response.userType;
+      this.userID = response.id;
+      this.displayCurriculum(response.id);
     });
   }
 
   // Display the list of Curriculum for Stakeholders
-  displayCurriculum() {
+  // Has userID parameter only for the checkSubmission Function
+  displayCurriculum(userID: any) {
     this.apiService.displayCurriculum().subscribe({
       next: (data) => {
+        // Manages the submissions array before display on the table
+        this.checkSubmission(userID, data, this.allFeedbacks);
+        
         console.log("Display Successful");
         this.curriculums = data;
       },
@@ -37,5 +47,41 @@ export class VoteComponent implements OnInit{
         console.log("Display Failed", err);
       }
     });
+  }
+
+  // Get all data from the VOTE Table without filter based on USER ID
+  getAllFeedbacks() {
+    this.apiService.getAllFeddbacks().subscribe({
+      next: (data) => {
+        this.allFeedbacks = data;
+      },
+      error: (err) => {
+        console.log("Display Failed", err);
+      }
+    });
+  }
+
+  // To check IF the USER have submitted a FEEDBACK on a CURRICULUM
+  checkSubmission(userID: any, curriculums: any, feedbacks: any) {
+    // Access the CURRICULUM Table
+    for (const curriculum of curriculums) {
+      let tempChecker = 0;
+
+      // Access the VOTE Table
+      for (const feedback of feedbacks) {
+        // Check IF the current feedback has the current CURRICULUM ID and USER ID
+        if (feedback.fk_vote_user_id === userID && feedback.fk_vote_curr_id === curriculum.id) {
+          tempChecker = 1;
+          this.submissions.push("Submitted");
+          break;
+        }
+      }
+
+      // To identify if the USER have not submitted a feedback
+      if (tempChecker === 0) {
+        tempChecker = 0;
+        this.submissions.push("Not Submitted");
+      }
+    }
   }
 }
